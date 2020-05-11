80f8d12cb749dc1eae4cfbad09c4f1269913218c
dohaicuong <beatyshot@gmail.com>
2020-05-12 00:49:25 +1000

    about page - WIP

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { Paper, Typography, makeStyles } from '@material-ui/core'
+import AboutSVG from 'resources/about.svg'
 
 const About = () => {
+  const classes = useStyles()
+
   return (
-    <div className='AboutPage'>
-      About page
-    </div>
+    <Paper className='AboutPage'>
+      <Typography variant='h5' className={classes.title}>
+        About
+      </Typography>
+      <img src={AboutSVG} className={classes.image} alt='Contact Eric Do' />
+    </Paper>
   )
 }
 export default About
+
+const useStyles = makeStyles(theme => ({
+  title: {
+    padding: theme.spacing(3)
+  },
+  image: {
+    width: '100%',
+    height: 'auto'
+  },
+}))
